e5a8d4a453ab203ef7e909dd277e8a0743f41477
dammina <dmsahabandu@gmail.com>
2015-03-03 00:51:40 +0530

    no of nodes

--- client/src/main/java/lk/filetributed/client/Client.java
+++ client/src/main/java/lk/filetributed/client/Client.java
@@ -28,6 +28,7 @@ public class Client extends Node{
 
     public void initialize() {
         String result = BootstrapConnector.connectToBootstrap(SERVER_NAME, PORT, CLIENT_IP, CLIENT_PORT, USERNAME);
+        process(result);
     }
     public List<FileTableEntry> searchFile(String filename) {
         return this.fileTable.searchTable(filename);
@@ -36,6 +37,37 @@ public class Client extends Node{
     public static void main(String[] args) {
         Client client = new Client();
 
+    }
+
+    public static void process(String server_response){
+        String[] response_data = server_response.split(" ");
+        if (!response_data[1].equals("REGOK")) {
+            System.err.println("Registration Failed. Try Again!");
+            System.exit(0);
+        }
+
+        int no_nodes = Integer.parseInt(response_data[2]);
+
+        switch (no_nodes) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 9999:
+                System.out.println("failed, there is some error in the command");
+                break;
+            case 9998:
+                System.out.println("failed, already registered to you, unregister first");
+                break;
+            case 9997:
+                System.out.println("failed, registered to another user, try a different IP and port");
+                break;
+            case 9996:
+                System.out.println("failed, can’t register. BS full.");
+                break;
+        }
     }
 
 
