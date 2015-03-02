a77fc15e996e86d60f30d77dd359eb2930a47e83
dammina <dmsahabandu@gmail.com>
2015-03-02 23:57:19 +0530

    ipTable sharing methods

--- model/src/main/java/lk/filetributed/model/IPTable.java
+++ model/src/main/java/lk/filetributed/model/IPTable.java
@@ -22,8 +22,41 @@ public class IPTable {
         entries.add(newEntry);
     }
 
+    public boolean searchClusterID(String clusterid) {
+        for (Iterator<TableEntry> iterator = entries.iterator(); iterator.hasNext();) {
+            TableEntry entry = iterator.next();
+            if (entry.getClusterID().equals(clusterid))
+                return false;
+        }
+        return true;
+    }
+
     public void removeTableEntry(TableEntry oldEntry) {
         entries.remove(oldEntry);
     }
 
+    public String convertToString(){
+        String entrylist = "";
+        for (Iterator<TableEntry> iterator = entries.iterator(); iterator.hasNext();){
+            entrylist += iterator.next().toString()+" ";
+        }
+        return entrylist;
+    }
+
+    public void setEntries(String entrylist, String clusterID) {
+        String[] entries = entrylist.trim().split(" ");
+        TableEntry newEntry;
+        String[] data;
+        for(String entry: entries){
+            data = entry.split(":");
+            if(data[2] == clusterID){
+                newEntry = new TableEntry(entry.split(":")[0],entry.split(":")[1],entry.split(":")[2]);
+                addTableEntry(newEntry);
+            }
+            if(searchClusterID(data[2])){
+                newEntry = new TableEntry(entry.split(":")[0],entry.split(":")[1],entry.split(":")[2]);
+                this.entries.add(newEntry);
+            }
+        }
+    }
 }
