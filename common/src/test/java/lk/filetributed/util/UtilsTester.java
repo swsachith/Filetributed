d037a3b68c9680613ecec0738fed44176dafad51
Sachith Withana <swsachith@gmail.com>
2015-03-03 00:43:11 +0530

    adding utils tester

--- common/src/test/java/lk/filetributed/util/UtilsTester.java
+++ common/src/test/java/lk/filetributed/util/UtilsTester.java
@@ -1,4 +1,32 @@
 package lk.filetributed.util;
 
+import junit.framework.Assert;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 public class UtilsTester {
+    private static Logger logger = Logger.getLogger(UtilsTester.class);
+
+
+    private Utils utils;
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testEncryptData() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        int clusterID = Utils.getClusterID("127.0.0.1", 3343, 3);
+        Assert.assertNotNull(clusterID);
+        logger.info(clusterID);
+
+    }
+
+
+
 }
