69a4d430d9ba837b9e332759cd2a51e746ea0bb5
dammina <dmsahabandu@gmail.com>
2015-03-03 17:52:13 +0530

    process method modified

--- client/src/main/java/lk/filetributed/client/Client.java
+++ client/src/main/java/lk/filetributed/client/Client.java
@@ -9,10 +9,7 @@ import lk.filetributed.util.Utils;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.List;
 
 
@@ -20,12 +17,12 @@ public class Client extends Node{
 
     private static Logger logger = Logger.getLogger(Client.class);
 
-    private static final String SERVER_NAME = "127.0.0.1";
+    private static final String SERVER_NAME = "192.168.43.77";
     private static final int PORT = 9889;
 
-    private static final String CLIENT_IP = "147.0.0.1";
+    private static final String CLIENT_IP = "192.168.43.208";
     private static final int CLIENT_PORT = 9888;
-    private static final String USERNAME = "sachith";
+    private static final String USERNAME = "adas";
     private static final int NO_CLUSTERS = 3;
 
     private static final String[] FILE_NAMES = {"Adventures of Tintin","Jack and Jill"};
@@ -43,7 +40,7 @@ public class Client extends Node{
     public void initialize() {
         String result = BootstrapConnector.connectToBootstrap(SERVER_NAME, PORT, CLIENT_IP, CLIENT_PORT, USERNAME);
         try {
-            process(result);
+            response_tokenizer(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -80,7 +77,7 @@ public class Client extends Node{
         Client client = new Client();
     }
 
-    public static void process(String server_response) throws IOException {
+    public void response_tokenizer(String server_response) throws IOException {
         String[] response_data = server_response.split(" ");
         if (!response_data[1].equals("REGOK")) {
             System.err.println("Registration Failed. Try Again!");
@@ -88,46 +85,17 @@ public class Client extends Node{
         }
 
         int no_nodes = Integer.parseInt(response_data[2]);
-        String RECIEVED_IP_01;
-        String RECIEVED_IP_02;
-        int RECIEVED_PORT_01;
-        int RECIEVED_PORT_02;
-        int clusterID01;
+
 
         switch (no_nodes) {
             case 0:
+                process();
                 break;
             case 1:
-                RECIEVED_IP_01 = response_data[3];
-                RECIEVED_PORT_01 = Integer.parseInt(response_data[4]);
-                clusterID01 = Utils.getClusterID(RECIEVED_IP_01, RECIEVED_PORT_01, NO_CLUSTERS);
-                Node node01 = new Node(RECIEVED_IP_01,RECIEVED_PORT_01,clusterID01);
-                String JOIN_MSG = JoinProtocol.getJoinMessage(CLIENT_IP,CLIENT_PORT);
-
-                DatagramSocket clientSocket = new DatagramSocket();
-                InetAddress IPAddress = InetAddress.getByName(RECIEVED_IP_01);
-                byte[] sendData = new byte[1024];
-                byte[] receiveData = new byte[1024];
-                sendData = JOIN_MSG.getBytes();
-
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, RECIEVED_PORT_01);
-                clientSocket.send(sendPacket);
-
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                clientSocket.receive(receivePacket);
-
-
-                /*TableEntry tableEntry = new TableEntry(node01.getIpAddress(),node01.getPort()+"",node01.getClusterID());
-
-                IPTable ipTable = new IPTable();
-                ipTable.addTableEntry(tableEntry);*/
-
+                process(response_data[3], Integer.parseInt(response_data[4]));
                 break;
             case 2:
-                RECIEVED_IP_01 = response_data[3];
-                RECIEVED_PORT_01 = Integer.parseInt(response_data[4]);
-                RECIEVED_IP_02 = response_data[5];
-                RECIEVED_PORT_02 = Integer.parseInt(response_data[6]);
+                process(response_data[3], Integer.parseInt(response_data[4]), response_data[5], Integer.parseInt(response_data[6]));
                 break;
             case 9999:
                 System.out.println("failed, there is some error in the command");
@@ -142,6 +110,38 @@ public class Client extends Node{
                 System.out.println("failed, can’t register. BS full.");
                 break;
         }
+    }
+    public void process(){
+
+    }
+    public void process(String RECIEVED_IP, int RECIEVED_PORT) throws IOException {
+        int clusterID;
+
+        clusterID = Utils.getClusterID(RECIEVED_IP, RECIEVED_PORT, NO_CLUSTERS);
+        Node node = new Node(RECIEVED_IP,RECIEVED_PORT,clusterID);
+        String JOIN_MSG = JoinProtocol.getJoinMessage(CLIENT_IP,CLIENT_PORT);
+
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName(RECIEVED_IP);
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        sendData = JOIN_MSG.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, RECIEVED_PORT);
+        clientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+
+        /*TableEntry tableEntry = new TableEntry(node01.getIpAddress(),node01.getPort()+"",node01.getClusterID());
+
+                IPTable ipTable = new IPTable();
+                ipTable.addTableEntry(tableEntry);*/
+
+    }
+    public void process(String RECIEVED_IP_01, int RECIEVED_PORT_01,String RECIEVED_IP_02, int RECIEVED_PORT_02){
+        int clusterID01;
+        int clusterID02;
     }
 
 
