00733f4ed4a40d741641aa5b0942e01070b94faf
catchsudheera <catchsudheera@gmail.com>
2015-04-08 12:48:24 +0530

    added specifying number of instances support

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-gnome-terminal --tab -e "tail -f s" --tab -e "java -jar client/target/Client-1.0-SNAPSHOT-jar-with-dependencies.jar client1.xml"
-sleep 2
-gnome-terminal --tab -e "java -jar client/target/Client-1.0-SNAPSHOT-jar-with-dependencies.jar client2.xml"
-sleep 2
-gnome-terminal --tab -e "java -jar client/target/Client-1.0-SNAPSHOT-jar-with-dependencies.jar client3.xml"
-sleep 2
+max=$1
+for i in `seq 1 $max`
+do
+    gnome-terminal --tab -e "tail -f s" --tab -e "java -jar client/target/Client-1.0-SNAPSHOT-jar-with-dependencies.jar client"$i".xml"
+    sleep 3
+    echo "Node" $i "started with configuration client"$i".xml"
+done
